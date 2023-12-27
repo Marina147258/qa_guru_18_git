91e649c558750abd72c000983ea663cdfee0fd1e
Marina Ostapenko <121380144+Marina147258@users.noreply.github.com>
2023-12-27 22:42:29 +0300

    Update Main.java

--- src/main/java/guru_qa/Main.java
+++ src/main/java/guru_qa/Main.java
@@ -1,7 +1,10 @@
 package guru_qa;
 
 public class Main {
+
+    public static String msg = "Hello qa.guru!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!!!!!");
+        System.out.println(msg);
     }
 }
