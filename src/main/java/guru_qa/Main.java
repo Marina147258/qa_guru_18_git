6e14b91150048c56bf1265e13d6ca26357425b3f
Marina Ostapenko <121380144+Marina147258@users.noreply.github.com>
2023-12-27 23:13:26 +0300

    Update Main.java

--- src/main/java/guru_qa/Main.java
+++ src/main/java/guru_qa/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static String msg = "Hello qa.guru!";
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
